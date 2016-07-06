b6c1de362b4b90d5cfe88d99c678e9bf16de3e0c
johnbrannstrom <john.brannstrom@gmail.com>
2016-07-06 22:29:40 +0200

    Added settingsa

--- src/settings.py
+++ src/settings.py
@@ -15,16 +15,15 @@ import yaml
 class Settings:
     """Settings container."""
 
-    @staticmethod
-    def load_settings_from_yaml():
+    def load_settings_from_yaml(self):
         """Set all system constants from YAML file."""
         with open('zipatoserver.conf', 'r') as f:
             constants = yaml.load(f)
         for constant, value in constants:
             if constant == 'HTTP_PATH':
-                self._HTTP_PATH = value
+                Settings._HTTP_PATH = value
             else:
-            Settings.__dict__[constant] = value
+                Settings.__dict__[constant] = value
 
     _HTTP_PATH = None
     @property
