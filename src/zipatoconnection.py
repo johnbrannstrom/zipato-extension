d5a26ef1a6fdd95a0bd0dfd9d848531db0410902
John Brännström <john.brannstrom@gmail.com>
2016-09-29 19:20:21 +0200

    Update zipatoconnection.py

--- src/zipatoconnection.py
+++ src/zipatoconnection.py
@@ -18,6 +18,12 @@ class ZipatoConnection(Settings):
        :param str ep: Target ep.
        :param str ep: Target apikey.
        :param bool ep: Status value to set the sensor to.
+       :rtype: int
+       :returns: Status of the HTTP request to the Zipato cloud.
        
        """
-       
+       command = ("https://my.zipato.com/zipato-web/remoting/attribute/se"
+                  "t?serial={}&ep={}&apiKey={}&state={}")
+       command = command.format(self.ZIPATO_SERIAL, ep, apikey, str(status))
+       response = requests.get(command)
+       return response.status_code
