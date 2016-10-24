d60803503395ec767527f15a82322d6f0ce22f63
John <john.brannstrom@gmail.com>
2016-10-24 07:44:18 +0200

    More docker.

--- docker/build_zipato_ext_image.sh
+++ docker/build_zipato_ext_image.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+RUN git clone https://github.com/johnbrannstrom/zipato-extension -b master \
+--single-branch /tmp/zipato-extension
 docker build . --no-cache -t zipato-extension
+RUN rm -Rf /tmp/zipato-extension
