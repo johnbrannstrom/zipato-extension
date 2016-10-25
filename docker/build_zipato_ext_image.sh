8f85f70724c8179b15cf569e6493f13c971c825e
John <john.brannstrom@gmail.com>
2016-10-25 06:30:01 +0200

    More docker.

--- docker/build_zipato_ext_image.sh
+++ docker/build_zipato_ext_image.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 git clone https://github.com/johnbrannstrom/zipato-extension -b master \
 --single-branch zipato-extension
 docker build . --no-cache -t zipato-extension
--- docker/spin_up_zipato_ext_container.sh
+++ docker/spin_up_zipato_ext_container.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/sh -x
 docker rm zipato-extension
 docker create -ti --name zipato-extension -v /var/log:/mnt/host/var/log \
--v /etc:/mnt/host/etc ubuntu:16.04 /bin/bash
+-v /etc:/mnt/host/etc zipato-extension:latest /bin/bash
 docker start zipato-extension
