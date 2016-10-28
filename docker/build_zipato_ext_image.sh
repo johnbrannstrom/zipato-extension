94e2b4b7ebf32d96e60a8f8b871e51744b1b7c70
John <john.brannstrom@gmail.com>
2016-10-29 01:51:56 +0200

    More docker.

--- docker/build_zipato_ext_image.sh
+++ docker/build_zipato_ext_image.sh
@@ -1,5 +1,32 @@
-#!/bin/sh -x
-git clone https://github.com/johnbrannstrom/zipato-extension -b master \
+#!/bin/bash -x
+
+# Get command line options
+BRANCH="master"
+NO_CACHE=false
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -b|--branch)
+        BRANCH="$2"
+        shift # past argument
+        ;;
+        --no-cache)
+        NO_CACHE=true
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+    shift # past argument or value
+done
+
+# Build image
+git clone https://github.com/johnbrannstrom/zipato-extension -b "$BRANCH" \
 --single-branch zipato-extension
-docker build . --no-cache -t zipato-extension
+if [ "$NO_CACHE" = true ]; then
+    docker build . -t zipato-extension --no-cache
+else
+    docker build . -t zipato-extension
+fi
 rm -Rf zipato-extension
--- docker/spin_up_zipato_ext_container.sh
+++ docker/spin_up_zipato_ext_container.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/bash -x
 docker rm zipato-extension
 docker create -ti --name zipato-extension -v /var/log:/mnt/host/var/log \
 -v /etc:/mnt/host/etc zipato-extension:latest /bin/bash
