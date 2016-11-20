33a05854317e44f6a1a4a1e65503a44899e06ba1
John <john.brannstrom@gmail.com>
2016-11-20 14:14:41 +0100

    docker

--- docker/spin_up_zipato_ext_container.sh
+++ docker/spin_up_zipato_ext_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Parse command line options
-NAME="zipato-extension"
+NAME=""
 IMAGE="zipato-extension"
 PORT="80"
 while [[ $# -gt 0 ]]
@@ -22,10 +22,9 @@ do
         -h|--help)
         echo "Usage: spin_up_zipato_ext_container.sh [-i] [-p] [-h]"
         echo -e "\nOptional arguments:"
-        echo "-n --name:  Container name."
-        echo "            Default value: zipato-extension."
         echo "-i --image: Image name to build container from."
         echo "            Default value: zipato-extension."
+        echo "-n --name:  Container name. Default value is same as image name."
         echo "-p --port:  Expose port to outside. Default value: 80"
         echo "-h --help:  Display this help."
         echo ""
@@ -37,12 +36,22 @@ do
     esac
     shift # past argument or value
 done
+if [ "${NAME}" == "" ]; then
+    NAME="${IMAGE}"
+fi
 
 # Delete old container
 docker stop "$NAME"
 docker rm "$NAME"
-# Create new container
-docker create -ti --name ${NAME} -p ${PORT}:${PORT} --net=host \
+# Create the create new container command
+COMMAND="docker create -ti --name ${NAME} -p ${PORT}:${PORT} --net=host \
+-p 23:23 \
 -v /var/log:/mnt/host/var/log \
--v /etc:/mnt/host/etc ${IMAGE}:latest
+-v /etc:/mnt/host/etc"
+# Add the image to the command
+COMMAND="${COMMAND} ${IMAGE}:latest"
+echo "${COMMAND}"
+# Execute command
+eval $COMMAND
+# Start the container
 docker start ${IMAGE}
